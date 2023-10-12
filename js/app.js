d5e3259d067e6b7992f322acd058273ab9b9bf8d
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2023-10-11 20:39:58 -0700

    Add functionality for code problem #4

--- js/app.js
+++ js/app.js
@@ -73,12 +73,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let theSum = 0;
 
+  for (let i = 0; i <= sumArr.length-1; i++) {
+    theSum = sum(theSum, sumArr[i])[0];
+  }
+
+  return [theSum, sumArr.join(',') + ' was passed in as an array of numbers, and ' + theSum + ' is their sum.'];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
